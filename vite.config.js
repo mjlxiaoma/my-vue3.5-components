aaefc921ade9bf3b9d95c6ef70aee3a5ad0f9325
小马同学 <89441679+m2075498242@users.noreply.github.com>
2025-06-20 17:27:31 +0800

    chore:更新依赖库

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        dts({
+            include: ['src/components/**/*.vue', 'src/components/**/*.ts', 'src/index.ts'],
+            beforeWriteFile: (filePath, content) => {
+                return {
+                    filePath: filePath.replace('/src/', '/'),
+                    content
+                };
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'MyVueComponents',
+            fileName: (format) => `my-vue-components.${format}.js`
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                },
+                exports: 'named',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === 'style.css')
+                        return 'style.css';
+                    return assetInfo.name;
+                }
+            }
+        }
+    }
+});
